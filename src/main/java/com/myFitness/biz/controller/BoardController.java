1da6b35b439d5c5f2d05b757ed2e32c11498670d
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-08 11:02:02 +0900

    update Controller, html

---
+++ src/main/java/com/myFitness/biz/controller/BoardController.java
@@ -0,0 +1,103 @@
+package com.myFitness.biz.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/allBoardList")
+	public String allBoardListView() {
+		
+		return "board/allBoardList";
+	}
+	
+	@GetMapping("/challengeBoardList")
+	public String challengeBoardListView() {
+		
+		return "board/challengeBoardList";
+	}
+	
+	@GetMapping("/deleteBoard")
+	public String deleteBoardView() {
+		
+		return "board/deleteBoard";
+	}
+	
+	@GetMapping("/deleteBoardCheck")
+	public String deleteBoardCheckView() {
+		
+		return "board/deleteBoardCheck";
+	}
+	
+	@GetMapping("/freeBoardList")
+	public String freeBoardListView() {
+		
+		return "board/freeBoardList";
+	}
+	
+	@GetMapping("/getBoard")
+	public String gerBoardView() {
+		
+		return "board/getBoard";
+	}
+	
+	@GetMapping("/getNoticeBoard")
+	public String getNoticeBoardView() {
+		
+		return "board/getNoticeBoard";
+	}
+	
+	@GetMapping("/insertBoard")
+	public String insertBoardView() {
+		
+		return "board/insertBoard";
+	}
+	
+//	@PostMapping("/insertBoard")
+//	public String insertBoard() {
+//		
+//		boardService.insertBoard(board);
+//		
+//		return "redirect:board/allBoardList";
+//	}
+	
+	@GetMapping("/noticeBoardList")
+	public String noticeBoardListView() {
+		
+		return "board/noticeBoardList";
+	}
+	
+	@GetMapping("/reportPage")
+	public String reportPageView() {
+		
+		return "board/reportPage";
+	}
+	
+	@GetMapping("/updateBoard")
+	public String updateBoardView() {
+		
+		return "board/updateBoard";
+	}
+	
+	@GetMapping("/videoBoardList")
+	public String videoBoardListView() {
+		
+		return "board/videoBoardList";
+	}
+	
+	@GetMapping("/reportBoardList")
+	public String reportBoardListView() {
+		
+		return "board/reportBoardList";
+	}
+	
+	@GetMapping("/categoryList")
+	public String categoryListView() {
+		
+		return "board/categoryList";
+	}
+	
+}
