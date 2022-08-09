c1c5eebbd0a40b9b29dd6dbf50f2e1923ee15950
Jiyun <liiiiim930405@naver.com>
2022-08-09 18:29:33 +0900

    package update

--- src/main/java/com/myFitness/persistence/BoardRepository.java
+++ src/main/java/com/myFitness/persistence/BoardRepository.java
@@ -2,7 +2,7 @@ package com.myFitness.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.myfitness.domain.Board;
+import com.myFitness.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 
