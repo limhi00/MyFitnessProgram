9a14f5f108ebed2294cc0e11f48dd6c9cf25af08
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-11 14:30:23 +0900

    [commit] Board 엔티티 수정.

--- src/main/java/com/myfitness/domain/Board.java
+++ src/main/java/com/myfitness/domain/Board.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -23,6 +24,8 @@ public class Board {
 	
 	private String title;
 	private String writer;
+	
+	@Column(nullable = false) // null 값 비허용
 	private String content;			  
 	
 	@Column(name = "createdate", updatable = false) // DB 컬럼명 CREATE_DATE => CREATEDATE
@@ -31,8 +34,8 @@ public class Board {
 	@Column(updatable = false, columnDefinition = "number default 0")
 	private Long cnt; // 조회수
 	
-	@ManyToOne
-	@JoinColumn(name="mid", nullable=false, updatable=false)
-	private Member member;
+//	@ManyToOne
+//	@JoinColumn(name="mid", nullable=false, updatable=false)
+//	private Member member;
 	
 }
