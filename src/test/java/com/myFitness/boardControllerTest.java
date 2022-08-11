511fe334fc00c7bc2e86fa3ff83f5ff9a0107cd0
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-11 10:03:18 +0900

    [commit] Board 엔티티 수정

--- src/test/java/com/myFitness/boardControllerTest.java
+++ src/test/java/com/myFitness/boardControllerTest.java
@@ -1,38 +1,32 @@
 package com.myFitness;
 
-
+import java.time.LocalDateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.myFitness.domain.Board;
-import com.myFitness.persistence.BoardRepository;
-
-
+import com.example.springboot.model.Board;
+import com.example.springboot.repository.BoardRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class boardControllerTest {
+public class BoardControllerTest {
 	
 	@Autowired 
 	private BoardRepository boardRepo;
-	
+
 	@Test
 	public void boardInsertTest() {
 		for(int i=1; i<=10; i++) {
 			Board board = new Board();
-			//board.setSeq(1L);
-			board.setTitle("ㅎㅇㅎㅇ " + i);
-			board.setWriter("테스트멤버");
+			board.setTitle("안녕하세요." + i);
+			board.setWriter("장동규");
 			board.setContent("테스트내용 " + i);
-			board.setCreateDate(null);
 			board.setCnt(0L);
-			board.setUp(0L);
+			board.setCreateDate(LocalDateTime.now()); // 2022-08-10 14:33:51.838953 
 			
 			boardRepo.save(board);
 		}
 	}
-
 }
