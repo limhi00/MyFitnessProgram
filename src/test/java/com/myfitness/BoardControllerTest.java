a2669bde1f68aa8dc7b0a469719c8a96a38f9be0
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-11 14:25:17 +0900

    commit Board 엔티티 수정

--- src/test/java/com/myfitness/BoardControllerTest.java
+++ src/test/java/com/myfitness/BoardControllerTest.java
@@ -1,5 +1,6 @@
 package com.myfitness;
 
+
 import java.time.LocalDateTime;
 
 import org.junit.Test;
@@ -11,24 +12,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.myfitness.domain.Board;
 import com.myfitness.persistence.BoardRepository;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoardControllerTest {
 	
 	@Autowired 
 	private BoardRepository boardRepo;
-
+	
 	@Test
 	public void boardInsertTest() {
 		for(int i=1; i<=10; i++) {
 			Board board = new Board();
 			board.setTitle("안녕하세요." + i);
-			board.setWriter("장동규");
-			board.setContent("테스트내용 " + i);
+			board.setWriter("홍길동");
+			board.setContent("테스트내용 " + i); 
+			board.setCreateDate(LocalDateTime.now());
 			board.setCnt(0L);
-			board.setCreateDate(LocalDateTime.now()); // 2022-08-10 14:33:51.838953 
 			
 			boardRepo.save(board);
+
 		}
 	}
+
 }
