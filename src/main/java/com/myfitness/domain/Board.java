07230e85dc1bc6fae783f1b85346bcc989a9e42a
user <user@DESKTOP-LB127L3>
2022-08-11 15:20:20 +0900

    update

--- src/main/java/com/myfitness/domain/Board.java
+++ src/main/java/com/myfitness/domain/Board.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 public class Board {
 	@Id
 	@GeneratedValue
-	private Long bseq;
+	private Long id;
 	
 	private String title;
 	private String writer;
@@ -36,6 +36,6 @@ public class Board {
 	
 	@ManyToOne
 	@JoinColumn(name="mid", nullable=false, updatable=false)
-	private Member member;
+ 	private Member member;
 	
 }
