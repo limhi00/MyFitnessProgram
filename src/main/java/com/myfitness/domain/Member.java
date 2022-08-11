fcda6cb9b03474562264848615d2c901e2e43685
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-11 14:59:38 +0900

    commit

--- src/main/java/com/myfitness/domain/Member.java
+++ src/main/java/com/myfitness/domain/Member.java
@@ -3,18 +3,13 @@ package com.myfitness.domain;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
