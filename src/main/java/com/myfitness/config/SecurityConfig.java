c41723a3c6ae459927778afb3e5e3b369976ddf2
DongkyuJang <trewqyuiop93@gmail.com>
2022-08-24 15:04:44 +0900

    test

--- src/main/java/com/myfitness/config/SecurityConfig.java
+++ src/main/java/com/myfitness/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
-            
+            //
         return http.build();
     }
 
